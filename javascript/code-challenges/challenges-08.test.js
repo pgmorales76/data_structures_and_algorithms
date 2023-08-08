f1b4742309f8b55127c11574db71fe6308e6b497
Peter G. Morales <pgmorales76@gmail.com>
2023-08-08 00:48:42 -0400

    Finished Challenge 4

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -103,6 +103,8 @@ Write a function named containsWorld that takes in a string or number of any len
 ------------------------------------------------------------------------------------------------ */
 
 const containsWorld = (input) => {
+  let includes_world = /(world)/g;
+  return includes_world.test(input);
   // Solution code here...
 };
 
